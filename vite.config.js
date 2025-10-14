ad878a53c86fbb04a662ba98109b55dfc5194ee6
Matheus José Rossieri <154281679+MatheusRossieri@users.noreply.github.com>
2025-10-14 00:36:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Base configurada para GitHub Pages (nome do repositório)
 export default defineConfig({
-  // Use base relative to avoid absolute paths that podem causar 404 no GitHub Pages
+  
   base: './',
   plugins: [react()],
 })
